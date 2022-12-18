16b63b130973ff46a55b3acddfa5dc8b6759fce9
luap42 <pstrobach15+luap42@gmail.com>
2022-12-18 18:12:30 +0100

    Add user table

--- webapp/views/admin.py
+++ webapp/views/admin.py
@@ -1,9 +1,23 @@
-from flask import Blueprint, render_template
-from flask_security import login_required
+from flask import Blueprint, render_template, abort
+from flask_security import login_required, current_user
+
+from ..models import db, User, Role
 
 admin_view = Blueprint('admin', __name__)
+
 
 @admin_view.route('/')
 @login_required
 def index():
     return render_template("admin/index.html")
+
+
+@admin_view.route('/user')
+@login_required
+def user():
+    if not current_user.has_privilege('manage_users'):
+        abort(404)
+
+    all_user = User.query.all()
+
+    return render_template("admin/user/index.html", all_user=all_user)
