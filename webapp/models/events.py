ccf4df34379652fffc60cc7d37e9962f98985c48
luap42 <pstrobach15+luap42@gmail.com>
2023-01-23 09:26:42 +0100

    Add supervisor columns to events

--- webapp/models/events.py
+++ webapp/models/events.py
@@ -7,3 +7,12 @@ class Event(db.Model):
 
     first_day = db.Column(db.DateTime())
     last_day = db.Column(db.DateTime())
+
+    supervising_user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    supervising_user = db.relationship('User', backref=db.backref('supervised_events', lazy='dynamic'))
+
+    supervising_role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
+    supervising_role = db.relationship('Role', backref=db.backref('supervised_events', lazy='dynamic'))
+
+    def is_supervisor(self, user):
+        return False
