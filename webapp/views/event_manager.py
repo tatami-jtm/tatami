e541fc52a6724a90aa694409bc45e28a29fdf5c2
luap42 <pstrobach15+luap42@gmail.com>
2023-05-18 16:41:26 +0200

    Fix issue with event check

--- webapp/views/event_manager.py
+++ webapp/views/event_manager.py
@@ -10,10 +10,17 @@ def check_and_apply_event(func):
     def inner_func(event, *args, **kwargs):
         event = Event.from_slug(event)
 
-        if not event.is_supervisor(current_user) and not current_user.has_privilege('create_tournaments'):
-            abort(404)
-
         g.event = event
+
+        return func(event, *args, **kwargs)
+    
+    inner_func.__name__ = func.__name__
+    return inner_func
+
+def check_is_event_supervisor(func):
+    def inner_func(event, *args, **kwargs):
+        if not g.event.is_supervisor(current_user) and not current_user.has_privilege('create_tournaments'):
+            abort(404)
 
         return func(event, *args, **kwargs)
     
@@ -24,11 +31,13 @@ def check_and_apply_event(func):
 @eventmgr_view.route('/')
 @login_required
 @check_and_apply_event
+@check_is_event_supervisor
 def index(event):
     return render_template("event-manager/index.html")
 
 @eventmgr_view.route('/classes')
 @login_required
 @check_and_apply_event
+@check_is_event_supervisor
 def classes(event):
     return render_template("event-manager/classes/index.html")
