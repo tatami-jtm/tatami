78074460a50a974afa1df41f7d584ad568ba53a0
luap42 <pstrobach15+luap42@gmail.com>
2023-11-10 16:04:23 +0100

    Allow editing associations

--- webapp/views/event_manager.py
+++ webapp/views/event_manager.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, abort, flash, g, \
 from flask_security import login_required, current_user
 
 from ..models import db, Event, EventClass, DeviceRegistration, \
-    DevicePosition, EventRole
+    DevicePosition, EventRole, Association
 
 from datetime import datetime
 import time
@@ -162,6 +162,29 @@ def registrations():
 @check_is_event_supervisor
 def associations():
     return render_template("event-manager/associations/index.html")
+
+
+@eventmgr_view.route('/associations/<id>/edit')
+@login_required
+@check_and_apply_event
+@check_is_event_supervisor
+def edit_association(id):
+    association = Association.query.filter_by(id=id).one_or_404()
+    return render_template("event-manager/associations/edit.html", association=association)
+
+
+@eventmgr_view.route('/associations/<id>/edit', methods=["POST"])
+@login_required
+@check_and_apply_event
+@check_is_event_supervisor
+def update_association(id):
+    association = Association.query.filter_by(id=id).one_or_404()
+    association.short_name = request.form['short_name']
+    association.name = request.form['name']
+
+    db.session.commit()
+
+    return redirect(url_for('event_manager.edit_association', event=g.event.slug, id=association.id))
 
 
 @eventmgr_view.route('/devices')
