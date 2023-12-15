399ad4e7a62980e376b6022f4868907da977f881
luap42 <pstrobach15+luap42@gmail.com>
2023-12-15 22:23:25 +0100

    Assigning groups to mats

--- webapp/views/mod_global_list.py
+++ webapp/views/mod_global_list.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 from .event_manager import check_and_apply_event
 from .devices import check_is_registered
 
-from ..models import db
+from ..models import db, Group
 
 mod_global_list_view = Blueprint('mod_global_list', __name__)
 
@@ -18,4 +18,48 @@ def index():
         flash('Sie haben keine Berechtigung, hierauf zuzugreifen.', 'danger')
         return redirect(url_for('devices.index', event=g.event.slug))
     
-    return render_template("mod_global_list/index.html")
+    free_groups = []
+    for event_class in g.event.classes.filter_by(begin_fighting=True, ended_fighting=False).all():
+        free_groups += event_class.groups.filter_by(assigned=None).all()
+        free_groups += event_class.groups.filter_by(assigned=False).all()
+        print(free_groups)
+
+
+    current_group = None
+    if 'group' in request.values:
+        current_group = g.event.groups.filter_by(id=request.values['group']).one_or_404()
+
+    mats = g.event.device_positions.filter_by(is_mat=True).all()
+    
+    return render_template("mod_global_list/index.html", mats=mats, current_group=current_group, free_groups=free_groups)
+
+
+@mod_global_list_view.route('/group/<id>/edit', methods=['POST'])
+@check_and_apply_event
+@check_is_registered
+def update_group(id):
+    if not g.device.event_role.may_use_global_list:
+        flash('Sie haben keine Berechtigung, hierauf zuzugreifen.', 'danger')
+        return redirect(url_for('devices.index', event=g.event.slug))
+    
+    group = g.event.groups.filter_by(id=id).one_or_404()
+
+    if request.form['assignment'] == 'none':
+        group.assigned = False
+        group.assigned_to_position = None
+    else:
+        position = g.event.device_positions.filter_by(id=request.form['assignment']).one_or_404()
+        group.assigned = True
+        group.assigned_to_position = position
+
+    if 'marked_ready' in request.form:
+        if not group.marked_ready:
+            group.marked_ready_at = dt.now()
+            group.marked_ready = True
+    else:
+        group.marked_ready = False
+        group.marked_ready_at = None
+    
+    db.session.commit()
+
+    return redirect(url_for('mod_global_list.index', event=g.event.slug))
