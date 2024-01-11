7b8dd1222cee1522030ec8abe7e803f4c73ee54b
luap42 <pstrobach15+luap42@gmail.com>
2024-01-11 23:58:00 +0100

    Add pdftool for the neccessary pdf operations

---
+++ webapp/listslib/pdftool.py
@@ -0,0 +1,43 @@
+import io, sys
+from fpdf import FPDF
+from pypdf import PdfReader, PdfWriter
+import pypdfium2 as pdfium
+
+def load_file(filename):
+    return PdfReader(filename)
+
+def merge_files(old, new, *pages):
+    output = PdfWriter()
+    for i, page in enumerate(pages):
+        old.pages[page].merge_page(page2=new.pages[i])
+        output.add_page(old.pages[page])
+
+    return output
+
+def create_new():
+    pdf = FPDF()
+    pdf.add_page()
+    return pdf
+
+def prepare_new(obj):
+    return PdfReader(io.BytesIO(obj.output()))
+
+def _make_writer(obj):
+    writer = PdfWriter()
+    writer.append_pages_from_reader(obj)
+    return writer
+
+def write_file(obj, filename):
+    _make_writer(obj).write(filename)
+
+def make_image(obj):
+    output = io.BytesIO()
+    _make_writer(obj).write(output)
+    pdf = pdfium.PdfDocument(output)
+    bitmap = pdf[0].render(
+        scale = 5,
+        optimize_mode='lcd'
+    )
+    pil_image = bitmap.to_pil()
+    pil_image.show()
+    return pil_image
