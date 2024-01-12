2ffe6b880e87533c24423276e8ae09a0b2bd90c7
luap42 <pstrobach15+luap42@gmail.com>
2024-01-12 23:33:59 +0100

    Allow downloading as pdf

--- webapp/views/mod_list.py
+++ webapp/views/mod_list.py
@@ -12,7 +12,7 @@ from .. import helpers
 
 mod_list_view = Blueprint('mod_list', __name__)
 
-@mod_list_view.route('/display/<id>.png')
+@mod_list_view.route('/display/<id>/list.png')
 @check_and_apply_event
 @check_is_registered
 def display_image(id):
@@ -29,3 +29,24 @@ def display_image(id):
     image_io.seek(0)
 
     return send_file(image_io, mimetype='image/png')
+
+
+@mod_list_view.route('/display/<id>/list.pdf')
+@check_and_apply_event
+@check_is_registered
+def display_pdf(id):
+    if not g.device.event_role.may_use_global_list and not g.device.event_role.may_use_assigned_lists:
+        flash('Sie haben keine Berechtigung, hierauf zuzugreifen.', 'danger')
+        return redirect(url_for('devices.index', event=g.event.slug))
+    
+    group = g.event.groups.filter_by(id=id).one_or_404()
+    group_list = helpers.load_list(group)
+
+    output = io.BytesIO()
+
+    pdf = group_list.make_pdf(title=g.event.title, event_class=group.event_class.short_title, group=group.cut_title())
+    pdf_io = io.BytesIO()
+    pdf.write(pdf_io)
+    pdf_io.seek(0)
+
+    return send_file(pdf_io, mimetype='application/pdf')
