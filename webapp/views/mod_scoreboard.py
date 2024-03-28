3ebd63d1d9fa7a29ed55582e08a281c4372e557a
luap42 <pstrobach15+luap42@gmail.com>
2024-03-28 19:10:44 +0100

    Begin managed scoreboard

--- webapp/views/mod_scoreboard.py
+++ webapp/views/mod_scoreboard.py
@@ -15,3 +15,18 @@ def standalone():
         return redirect(url_for('devices.index', event=g.event.slug))
 
     return render_template("mod_scoreboard/standalone.html")
+
+
+@mod_scoreboard_view.route('/managed')
+@check_and_apply_event
+@check_is_registered
+def managed():
+    if not g.device.event_role.may_use_scoreboard or \
+        not g.device.event_role.may_use_assigned_lists:
+        flash('Sie haben keine Berechtigung, hierauf zuzugreifen.', 'danger')
+        return redirect(url_for('devices.index', event=g.event.slug))
+
+    g.mat = g.device.position
+    assigned_lists = g.mat.assigned_groups.filter_by(marked_ready=True).all()
+
+    return render_template("mod_scoreboard/managed.html", assigned_lists=assigned_lists)
