7f49cf20f76e9ad19c5b4b5a0c3336aa74424fce
luap42 <pstrobach15+luap42@gmail.com>
2024-04-02 16:19:10 +0200

    Fix error in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from webapp.app import db
+from webapp.app import app, db
 
-db.create_all()
+with app.app_context():
+    db.create_all()
+
 print("Database has been created. Start server and go to /setup to continue installation.")
