b1360384f97c13065ed06aaf054a02f6e70c8636
luap42 <pstrobach15+luap42@gmail.com>
2024-05-22 01:22:12 +0200

    Automatically stamp current migrations at setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from webapp.app import app, db
+from flask_migrate import stamp
 
 with app.app_context():
     db.create_all()
+    stamp('migrations')
 
 print("Database has been created. Start server and go to /setup to continue installation.")
