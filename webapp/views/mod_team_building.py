15ccee5417e434b25a35debfdfc75da561cc6b1d
luap42 <pstrobach15+luap42@gmail.com>
2024-10-25 22:28:34 +0200

    Begin team building UI

--- webapp/views/mod_team_building.py
+++ webapp/views/mod_team_building.py
@@ -23,3 +23,26 @@ def index():
         return redirect(url_for('devices.index', event=g.event.slug))
 
     return render_template("mod_team_building/index.html")
+
+
+@mod_team_building_view.route('/class/<id>')
+@check_and_apply_event
+@check_is_registered
+@check_event_is_in_team_mode
+def for_class(id):
+    if not g.device.event_role.may_use_placement_tool:
+        flash('Sie haben keine Berechtigung, hierauf zuzugreifen.', 'danger')
+        return redirect(url_for('devices.index', event=g.event.slug))
+
+    event_class = g.event.classes.filter_by(id=id).one_or_404()
+
+    if not event_class.begin_placement:
+        flash(f"Die Kampfklasse {event_class.title} wurde noch nicht freigegeben.", 'danger')
+        return redirect(url_for('mod_team_building.index', event=g.event.slug))
+    
+    current_team = None
+
+    if 'team' in request.values:
+        current_team = event_class.teams.filter_by(id=request.values['team']).one_or_404()
+
+    return render_template("mod_team_building/for_class.html", event_class=event_class, current_team=current_team)
