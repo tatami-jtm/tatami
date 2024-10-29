ad1275899f26d329a0dde885873db58d7e40ef1f
luap42 <pstrobach15+luap42@gmail.com>
2024-10-29 20:58:45 +0100

    Add initializer for team rows

--- webapp/views/mod_team_building.py
+++ webapp/views/mod_team_building.py
@@ -5,9 +5,9 @@ from datetime import datetime as dt
 
 from .event_manager import check_and_apply_event, check_event_is_in_team_mode
 from .devices import check_is_registered
-from .mod_placement import provide_classes_query
+from .mod_placement import provide_classes_query, _get_weight_classes
 
-from ..models import db, TeamRegistration
+from ..models import db, TeamRegistration, TeamRow
 
 mod_team_building_view = Blueprint('mod_team_building', __name__)
 
@@ -40,6 +40,9 @@ def for_class(id):
         flash(f"Die Kampfklasse {event_class.title} wurde noch nicht freigegeben.", 'danger')
         return redirect(url_for('mod_team_building.index', event=g.event.slug))
     
+    if not event_class.team_rows.count() > 0 and not event_class.use_proximity_weight_mode:
+        return redirect(url_for('mod_team_building.initialize', event=g.event.slug, id=event_class.id))
+    
     current_team = None
     registration_filter = None
     registrations = event_class.registrations
@@ -61,3 +64,68 @@ def for_class(id):
                 registrations = registrations.filter_by(team_registration_id=registration_filter)
 
     return render_template("mod_team_building/for_class.html", event_class=event_class, current_team=current_team, registration_filter=registration_filter, registrations=registrations)
+
+
+@mod_team_building_view.route('/class/<id>/initialize', methods=['GET', 'POST'])
+@check_and_apply_event
+@check_is_registered
+@check_event_is_in_team_mode
+def initialize(id):
+    if not g.device.event_role.may_use_placement_tool:
+        flash('Sie haben keine Berechtigung, hierauf zuzugreifen.', 'danger')
+        return redirect(url_for('devices.index', event=g.event.slug))
+
+    event_class = g.event.classes.filter_by(id=id).one_or_404()
+
+    if not event_class.begin_placement:
+        flash(f"Die Kampfklasse {event_class.title} wurde noch nicht freigegeben.", 'danger')
+        return redirect(url_for('mod_team_building.index', event=g.event.slug))
+    
+    if event_class.team_rows.count() > 0 or event_class.use_proximity_weight_mode:
+        return redirect(url_for('mod_team_building.for_class', event=g.event.slug, id=event_class.id))
+
+    weight_classes = _get_weight_classes(event_class)
+
+    if request.method == 'POST':
+        
+        if request.form['mode'] == 'predefined':
+            lower_row = None
+            match_order_key = 0
+            for cl in weight_classes:
+                lower_row = TeamRow(
+                    event = g.event,
+                    event_class = event_class,
+                    lower_row = lower_row,
+
+                    title = event_class.short_title + ' ' + cl[0],
+                    match_order_key = match_order_key,
+
+                    created_manually = False,
+                    assign_by_logic = True,
+                    min_weight = cl[1][0] * 1000 if cl[1][0] else None,
+                    max_weight = cl[1][1] * 1000 if cl[1][1] else None,
+                )
+                db.session.add(lower_row)
+                match_order_key += 1
+
+        elif request.form['mode'] == 'manual':
+            db.session.add(TeamRow(
+                event = g.event,
+                event_class = event_class,
+                lower_row = None,
+
+                title = event_class.short_title + ' ' + request.form['name'],
+                match_order_key = 0,
+
+                created_manually = True,
+                assign_by_logic = 'assign_by_logic' in request.form,
+                min_weight = int(float(request.form['min_weight']) * 1000) if request.form['min_weight'] else None,
+                max_weight = int(float(request.form['max_weight']) * 1000) if request.form['max_weight'] else None
+            ))
+            
+        db.session.commit()
+
+        return redirect(url_for('mod_team_building.for_class', event=g.event.slug, id=event_class.id))
+
+    return render_template("mod_team_building/initialize.html",
+                               event_class=event_class, weight_classes=weight_classes)
