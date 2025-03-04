29128702461b58439d7fb639c3576bb66d1b8cdc
luap42 <pstrobach15+luap42@gmail.com>
2025-03-04 15:37:52 +0100

    Remove a few unneccessary status messages

--- webapp/views/mod_placement.py
+++ webapp/views/mod_placement.py
@@ -465,8 +465,6 @@ def place(id, participant_id):
         db.session.commit()
         g.event.log(g.device.title, 'DEBUG', f'TN {participant.full_name} an Position #{participant.placement_index + 1} gesetzt.')
 
-        flash(f"TN {participant.full_name} erfolgreich an Position #{participant.placement_index + 1} gesetzt.", 'success')
-
         return redirect(url_for('mod_placement.for_class', event=g.event.slug, id=event_class.id, group=group.id))
 
     return render_template("mod_placement/participant/place.html", event_class=event_class, group=group, participant=participant, list_system=list_system)
@@ -489,7 +487,6 @@ def unplace(id, participant_id):
 
     db.session.commit()
 
-    flash(f"TN {participant.full_name} erfolgreich abgesetzt.", 'success')
     g.event.log(g.device.title, 'DEBUG', f'TN {participant.full_name} abgesetzt.')
 
     return redirect(url_for('mod_placement.for_class', event=g.event.slug, id=event_class.id, group=group.id))
